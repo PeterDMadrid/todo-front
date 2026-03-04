0be76ea20befdc0313e6e938b16f1df61a86cd67
PeterDMadrid <petermadrid0421@gmail.com>
2026-03-04 14:15:56 +0800

    asdasdasd

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex justify-center min-h-screen">
-      <Link href="/Homepage">Go to Notes</Link>
+      <Link href="/homepage">Go to Notes</Link>
     </div>
   );
 }
